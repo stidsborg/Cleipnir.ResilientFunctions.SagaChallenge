7d764b4a8ca9a086e99e155867f543eece2fa46b
stidsborg <stidsborg@cleipnir.net>
2022-11-22 18:08:48 +0100

    Removed solution to ResilientFunctionApproach

--- Challenge#3/ResilientFunctionApproach/OrderProcessor.cs
+++ Challenge#3/ResilientFunctionApproach/OrderProcessor.cs
@@ -17,18 +17,6 @@ public class OrderProcessor
     public async Task ProcessOrder(Order order, Scrapbook scrapbook, Context context)
     {
         var eventSource = await context.EventSource;
-
-        _messageBroker.Send(new ReserveFunds(order.OrderId, order.TotalPrice, scrapbook.TransactionId, order.CustomerId));
-        await eventSource.All.OfType<FundsReserved>().NextEvent(maxWaitMs: 5_000);
-
-        _messageBroker.Send(new ShipProducts(order.OrderId, order.CustomerId, order.ProductIds));
-        await eventSource.All.OfType<ProductsShipped>().NextEvent(maxWaitMs: 5_000);
-
-        _messageBroker.Send(new CaptureFunds(order.OrderId, order.CustomerId, scrapbook.TransactionId));
-        await eventSource.All.OfType<FundsCaptured>().NextEvent(maxWaitMs: 5_000);
-
-        _messageBroker.Send(new SendOrderConfirmationEmail(order.OrderId, order.CustomerId));
-        await eventSource.All.OfType<OrderConfirmationEmailSent>().NextEvent(maxWaitMs: 5_000);
     }
 
     public class Scrapbook : RScrapbook
